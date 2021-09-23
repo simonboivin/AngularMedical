207e6a47fa230f099c08271b0454baf775cbeb2c
Simon Boivin <sfp.boivin@gmail.com>
2021-09-23 16:06:38 +0200

    Ajout icone FontAwesome

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee, faShieldVirus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  faCoffee = faCoffee;
+  faShieldVirus = faShieldVirus;
   constructor() { }
 
   ngOnInit(): void {
