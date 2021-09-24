b77db2c2dd6257ead5ab1cb1de06253c3c2b351a
Simon Boivin <sfp.boivin@gmail.com>
2021-09-24 16:46:13 +0200

    bouton maj villes

--- src/app/villes/villes.component.ts
+++ src/app/villes/villes.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { Villes } from "../classes/villes";
 import { environment } from 'src/environments/environment';
+import { faSync } from '@fortawesome/free-solid-svg-icons';
 
 const httpOptions = {
   headers: new HttpHeaders(
@@ -20,11 +21,22 @@ const httpOptions = {
 
 export class VillesComponent implements OnInit {
 
+  faSync = faSync;
+
   villes: Array<Villes> = [];
+  @Input() nom: string = "";
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+
+    this.updateCities();
+
+  }
+
+  updateCities(): void {
+
+
     this.http.get<Villes[]>(environment.urlApi + "villes", httpOptions).subscribe(
       data => {
         this.villes = data;
