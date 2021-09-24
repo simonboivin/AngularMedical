04d7744e6be1a48c69a8f61f5276617682503c79
Simon Boivin <sfp.boivin@gmail.com>
2021-09-24 09:35:06 +0200

    Configuration Paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientsComponent } from './patients/patients.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "patients", component: PatientsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
