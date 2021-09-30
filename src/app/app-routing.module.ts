fab786dd900cb292728e4d9854bbde942c62eaa8
Simon Boivin <sfp.boivin@gmail.com>
2021-09-30 11:22:36 +0200

    Configuration Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { GuardianGuard } from './guardian.guard';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { PatientsComponent } from './patients/patients.component';
 import { VillesComponent } from './villes/villes.component';
 
 const routes: Routes = [
   { path: "login", component: LoginFormComponent },
-  { path: "", component: DashboardComponent },
-  { path: "patients", component: PatientsComponent },
-  { path: "villes", component: VillesComponent }
+  { path: "", component: DashboardComponent, canActivate: [GuardianGuard] },
+  { path: "patients", component: PatientsComponent, canActivate: [GuardianGuard] },
+  { path: "villes", component: VillesComponent, canActivate: [GuardianGuard] }
 ];
 
 @NgModule( {
   imports: [RouterModule.forRoot( routes )],
   exports: [RouterModule]
 } )
-  
+
 export class AppRoutingModule { }
